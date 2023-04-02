4ecc0f00faa8c22d7aecacdf54ff7349e396e72a
username <okch0310@naver.com>
2023-04-02 17:47:50 +0900

    [MOD] 게시글 목록 타이틀 font size 통일, 정렬 (반응형)

--- src/components/Board_header2/Board_header.js
+++ src/components/Board_header2/Board_header.js
@@ -1,12 +1,21 @@
 import "./board_header.scss";
 function Board_header(props) {
   return (
-    <div className="board_header_container">
+    <>
+    {/* <div className="board_header_container">
       <h4>{props.t1}</h4>
       <h4>{props.t2}</h4>
       <h4>{props.t3}</h4>
       <h4>{props.t4}</h4>
+    </div> */}
+
+    <div className="board_header_wrapper">
+      <span>{props.t1}</span>
+      <span>{props.t2}</span>
+      <span>{props.t3}</span>
     </div>
+    </>
+    
   );
 }
 
